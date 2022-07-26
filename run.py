e781e84eee7b510879593d1ac726e6e64d34780b
Morten W. Petersen <morphex@gmail.com>
2022-07-26 23:50:48 +0200

    Working on exit, and debug tools

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import socketserver, socket
 import sys
 import time
 import _thread, threading
-from sdt import DEBUG_PRINT
+from sdt import DEBUG_PRINT, INFO_PRINT
 
 listen_address, listen_port, remote_address, remote_port = sys.argv[1], \
 				int(sys.argv[2]), sys.argv[3], int(sys.argv[4])
@@ -12,12 +12,12 @@ listen_address, listen_port, remote_address, remote_port = sys.argv[1], \
 try:
     max_active_connections = int(sys.argv[5])
 except IndexError:
-    max_active_connections = 5
+    max_active_connections = 25
 
 try:
     activity_timeout_seconds = int(sys.argv[6])
 except IndexError:
-    activity_timeout_seconds = 10
+    activity_timeout_seconds = 5
 
 
 active_connections = 0
@@ -100,7 +100,8 @@ class SimpleHandler(socketserver.StreamRequestHandler):
                 pass
             if to_proxy:
                 last_activity = time.time()
-                DEBUG_PRINT("To proxy", to_proxy)
+                DEBUG_PRINT("Sending to proxy")
+                INFO_PRINT(to_proxy)
                 proxy_s.send(to_proxy)
                 to_proxy = bytes()
             try:
@@ -111,7 +112,8 @@ class SimpleHandler(socketserver.StreamRequestHandler):
                 pass
             if to_user:
                 last_activity = time.time()
-                DEBUG_PRINT("To user", to_user)
+                DEBUG_PRINT("Sending to user")
+                INFO_PRINT(to_user)
                 user_s.send(to_user)
                 to_user = bytes()
             if last_activity + activity_timeout_seconds < time.time():
@@ -135,6 +137,13 @@ class SimpleTCPProxy(socketserver.ThreadingMixIn, socketserver.TCPServer):
         global listen_address, listen_port
         socketserver.TCPServer.__init__(self, (listen_address, listen_port),
 					SimpleHandler)
+
+    def serve_forever(self, *arguments, **keywords):
+        try:
+            super().serve_forever(*arguments, **keywords)
+        finally:
+            self.socket.close()
+            #self.socket.shutdown(socket.SHUT_RDWR)
 
 server = SimpleTCPProxy()
 server.server_activate()
--- sdt.py
+++ sdt.py
@@ -3,8 +3,13 @@
 import sys
 
 DEBUG = True
+INFO = False
 
-def DEBUG_PRINT(*arguments):
+def DEBUG_PRINT(*arguments, DEBUG=DEBUG):
     if DEBUG:
+        print(*arguments, file=sys.stderr)
+
+def INFO_PRINT(*arguments, INFO=INFO):
+    if INFO:
         print(*arguments, file=sys.stderr)
 
